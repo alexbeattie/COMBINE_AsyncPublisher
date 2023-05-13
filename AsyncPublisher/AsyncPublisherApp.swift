551e69022b24a1a2e60e689515603751e2d62957
Alex Beattie <alexanderbeattie@mac.com>
2023-05-13 10:00:02 -0700

    Initial Commit

---
+++ AsyncPublisher/AsyncPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncPublisherApp.swift
+//  AsyncPublisher
+//
+//  Created by Alex Beattie on 5/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
