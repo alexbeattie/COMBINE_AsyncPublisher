ed1efc02564d2ff5daa0dd3539f1af685901c946
Alex Beattie <alexanderbeattie@mac.com>
2023-05-13 15:04:22 -0700

    Combine

--- AsyncPublisher/AsyncPublisherApp.swift
+++ AsyncPublisher/AsyncPublisherApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AsyncPublisherApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AsyncPublisherBootcamp()
         }
     }
 }
--- AsyncPublisher/ContentView.swift
+++ AsyncPublisher/ContentView.swift
@@ -6,21 +6,62 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+class AsyncPublisherDataManager {
+    @Published var myData: [String] = []
+    
+    func addData() async{
+        myData.append("Apple")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        myData.append("Banana")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
+        myData.append("Grape")
+        try? await Task.sleep(nanoseconds: 2_000_000_000)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+class AsyncPublisherBootcampViewModel: ObservableObject {
+    @Published var dataArray: [String] = []
+    
+    let manager = AsyncPublisherDataManager()
+    var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        addSubscribers()
+    }
+    private func addSubscribers() {
+        manager.$myData
+            .receive(on: DispatchQueue.main, options: nil)
+            .sink { dataArray in
+                self.dataArray = dataArray
+            }
+            .store(in: &cancellables)
+    }
+    
+    
+    func start() async {
+        await manager.addData()
+    }
+}
+struct AsyncPublisherBootcamp: View {
+    @StateObject private var viewModel = AsyncPublisherBootcampViewModel()
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(viewModel.dataArray, id: \.self) {
+                    Text($0).font(.headline)
+                }
+            }
+        }
+        .task {
+            await viewModel.start()
+        }
+    }
+}
+
+struct AsyncPublisherBootcamp_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AsyncPublisherBootcamp()
     }
 }
